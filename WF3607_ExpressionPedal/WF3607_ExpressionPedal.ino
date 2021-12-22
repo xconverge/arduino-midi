9f78bcffc2dec6dc44e4abb207d419f0df3a015f
Sean Kelly <sevendaystillsean@gmail.com>
2021-12-21 21:58:56 -0800

    Formatting

--- WF3607_ExpressionPedal/WF3607_ExpressionPedal.ino
+++ WF3607_ExpressionPedal/WF3607_ExpressionPedal.ino
@@ -6,27 +6,24 @@
 int lastVal = 0;
 int tempAnalog = 0;
 
-void setup() {
-  Serial.begin(300); 
-}
+void setup() { Serial.begin(300); }
 
 void controlChange(byte channel, byte control, byte value) {
-   midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
   MidiUSB.sendMIDI(event);
 }
 
 void loop() {
   // Read analog value
   tempAnalog = analogRead(expPin);
-  
+
   // Convert 10 bit to 7 bit
   tempAnalog = map(tempAnalog, 0, 1023, 0, 127);
   tempAnalog = constrain(tempAnalog, 0, 127);
 
   // Send pedal status
-  if(tempAnalog != lastVal)
-  {
-    // CC 11   (Our default value for expression pedal is using 
+  if (tempAnalog != lastVal) {
+    // CC 11   (Our default value for expression pedal is using
     //          MIDI control change number 11
     //          Change this to change control the change number)
     controlChange(1, 11, tempAnalog);
@@ -36,5 +33,5 @@ void loop() {
 
   // Store current value to be used laters
   lastVal = tempAnalog;
-  delay(5);  
+  delay(5);
 }
